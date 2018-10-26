4394ec8892b026f5c4138963d7ac423891e70c6f
Zhikui Guo <zguoch@gmail.com>
2018-10-26 21:31:30 +0200

    清理完毕

---
+++ ZH/clean.sh
@@ -0,0 +1,12 @@
+rm *.aux
+rm *.bbl
+rm *.blg
+rm *.loe
+rm *.lof
+rm *.log
+rm *.out
+rm *.soc
+rm *.gz
+rm *.lot
+rm *.thm
+rm *.toc
--- main.py
+++
@@ -1,7 +0,0 @@
-print('hellow world')
-import numpy as np
-import matplotlib.pyplot as plt
-plt.figure()
-x=np.linspace(-10,10,200)
-print(x)
-
